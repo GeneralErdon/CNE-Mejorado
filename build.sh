d857d26a355b964f83156fe0aa7ea0b91c85f793
GeneralErdon <leandrofermin@gmail.com>
2023-11-21 16:51:29 -0400

    Configuración inicial del Proyecto de elecciones

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+
+ruta=$(pwd) # Establece la ruta actual
+
+if [ ! -d "env" ]; then # verifica la existencia del entorno virtual
+    python3.11 -m virtualenv env
+fi
+if [ ! -d "logs" ]; then
+    mkdir -p $ruta/logs
+fi
+
+
+$ruta/env/bin/python -m pip install -r $ruta/requirements.txt
+$ruta/env/bin/python $ruta/manage.py migrate
+$ruta/env/bin/python $ruta/manage.py collectstatic --no-input
+
+#yarn --cwd $ruta
+#yarn --cwd $ruta build
---
+++ core/settings.py
@@ -0,0 +1,198 @@
+
+import os
+from pathlib import Path
+import environ
+from datetime import timedelta
+
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ = environ.Env()
+environ.read_env(BASE_DIR / ".env")
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+DEBUG = environ.bool("DJANGO_DEBUG")
+
+ALLOWED_HOSTS = ["*"]
+CORS_ALLOWED_ORIGINS = environ.list("DJANGO_CORS_ALLOWED_ORIGINS")
+
+CORS_ORIGIN_WHITELIST = CORS_ALLOWED_ORIGINS
+
+# Application definition
+
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
+    "jazzmin",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
+    "simple_history",
+    "drf_yasg",
+    "corsheaders",
+    "debug_toolbar",
+]
+
+MY_APPS = [
+    "core",
+    
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
+    'django.contrib.admindocs.middleware.XViewMiddleware',
+]
+
+ROOT_URLCONF = 'core.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': environ("POSTGRESQL_DB_NAME"),
+        'USER': environ("POSTGRESQL_DB_USER"),
+        'PASSWORD': environ("POSTGRESQL_DB_PASSWORD"),
+        'PORT': environ("POSTGRESQL_PORT"),
+        'HOST': environ("POSTGRESQL_HOST"),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'es-ES'
+
+TIME_ZONE = 'America/Caracas'
+
+USE_I18N = True
+
+USE_TZ = False
+
+TIME_INPUT_FORMATS = [
+    "%I:%M %p",
+]
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuracion de Apps de terceros
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_FILTER_BACKENDS": (
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    )
+}
+
+SIMPLE_HISTORY_ENFORCE_HISTORY_MODEL_PERMISSIONS = True
+
+SWAGGER_SETTINGS = {
+    "DOC_EXPANSION": 'none',
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=180),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=360),
+
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True
+    # 'UPDATE_LAST_LOGIN': True,
+}
+
+if not DEBUG:
+    ALLOWED_HOSTS = environ.list("DJANGO_ALLOWED_HOSTS")
+    
+    SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] = timedelta(hours=4)
+    SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'] = timedelta(hours=8)
+    
+    REST_FRAMEWORK["DEFAULT_PERMISSION_CLASSES"] = (
+        "rest_framework.permissions.IsAuthenticated", 
+        "rest_framework.permissions.DjangoModelPermissions",
+        )
+
+
+

38f02b5895dde2ccb61d25c70268a894aa0c3579
GeneralErdon <leandrofermin@gmail.com>
2023-12-08 09:36:04 -0400

    Arreglado el config de tailwind

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,9 +1,11 @@
 import AdbIcon from '@mui/icons-material/Adb'
+import { TextField } from '@mui/material'
 function App() {
 
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <AdbIcon  />
+    <div className="w-full h-[100vh] flex justify-center items-center bg-red-300">
+      <AdbIcon  /> a
+      <TextField className='bg-red-400' ></TextField>
 
     </div>
   )
